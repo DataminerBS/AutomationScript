f593d1848810aed6d689a45bda659047f9a9aa07
U10049432 <U10049432@WLT731060>
2023-03-28 13:11:52 +0100

    Add project files.

---
+++ Example1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Example1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Example1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4829A529-24A3-4EA7-98E8-82688FD6BE24")]
+[assembly: AssemblyVersion("1.0.0.0")]
