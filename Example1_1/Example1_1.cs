4bee4487a6f23ffbbf28ec9ae190b230387908ee
U10049432 <U10049432@WLT731060>
2023-03-28 15:25:18 +0100

    order dispatch

--- Example1_1/Example1_1.cs
+++ Example1_1/Example1_1.cs
@@ -55,6 +55,7 @@ namespace Example1_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room4;
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 
@@ -70,7 +71,7 @@ namespace Example1_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-
+			/**
 			engine.GenerateInformation("Hello World");
 
 			var dms = engine.GetDms();
@@ -81,7 +82,12 @@ namespace Example1_1
 			{
 				engine.GenerateInformation("Element: " + element.Name);
 			}
-	
+			**/
+
+			var order= OrderFactory.CreateOrder("order 1");
+			order.Dispatch("Bruno Sousa");
+
+
 		}
 	}
 }
